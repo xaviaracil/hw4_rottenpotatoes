8eeb6bf33befec73ee8f94816af53c2e98c0b415
Xavi Aracil <xavi@xadsolutions.com>
2012-06-19 21:55:10 +0200

    definition of 'should call the model method that search movies with given director'

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'find movies with same director' do
-    it 'should call the model method that search movies with given director'
+    it 'should call the model method that search movies with given director' do
+      post :same_director, {:id => 1}
+    end
     it 'should select the similar movies template for rendering'
     it 'should make the search results to that template'
   end
