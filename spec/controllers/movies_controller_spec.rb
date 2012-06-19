b4963403d119d04b2ae488c40616d48f4a0b3a18
Xavi Aracil <xavi@xadsolutions.com>
2012-06-19 22:37:57 +0200

    Refactoring

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,23 +2,24 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'find movies with same director' do
+    before :each do
+      @movie = FactoryGirl.build(:movie)
+    end
+    
     it 'should call the model method that search movies with given director' do
-      movie = FactoryGirl.build(:movie)
-      fake_results = [FactoryGirl.build(:movie), FactoryGirl.build(:movie)]
+      
+      Movie.should_receive(:find).with(@movie.id.to_s).and_return(@movie)
+      @movie.should_receive(:find_with_same_director)
 
-      Movie.should_receive(:find).with(movie.id.to_s).and_return(movie)
-      movie.should_receive(:find_with_same_director).and_return(fake_results)
-
-      post :same_director, {:id => movie.id}
+      post :same_director, {:id => @movie.id}
     end
     
     it 'should select the similar movies template for rendering' do
-      movie = FactoryGirl.build(:movie)
       
-      Movie.stub(:find).and_return(movie)
-      movie.stub(:find_with_same_director)
+      Movie.stub(:find).and_return(@movie)
+      @movie.stub(:find_with_same_director)
       
-      post :same_director, {:id => movie.id}
+      post :same_director, {:id => @movie.id}
       response.should render_template('same_director')
     end
     
