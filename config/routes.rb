fd823bc81ae5fcfdb39b874799d024cca07047f0
Xavi Aracil <xavi@xadsolutions.com>
2012-06-19 21:56:50 +0200

    Added route to same_director

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rottenpotatoes::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  match 'movies/:id/same_director' => 'movies#same_director', :as => :same_director
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
