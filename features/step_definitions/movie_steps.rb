899b6ba9b4b7d40ca21204a09d0952bbe0ade187
Xavi Aracil <xavi@xadsolutions.com>
2012-06-19 21:28:40 +0200

    Added step definition for: Then the director of "Alien" should be "Ridley Scott"

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,3 +4,8 @@ Given /^the following movies exist:$/ do |table|
     Movie.create! movie
   end
 end
+
+Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |title, director|
+  Movie.find_by_title(title).director.should == director
+end
+
