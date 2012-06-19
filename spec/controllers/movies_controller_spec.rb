803597d4a0e3296b0bf45ab7f5b2b5de5d218c34
Xavi Aracil <xavi@xadsolutions.com>
2012-06-19 21:49:30 +0200

    Created spec for "find movies with same director"

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'find movies with same director' do
+    it 'should call the model method that search movies with given director'
+    it 'should select the similar movies template for rendering'
+    it 'should make the search results to that template'
+  end
+end
