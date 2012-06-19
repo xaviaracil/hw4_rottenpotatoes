1f8c74dc4a5d353d0e1f0a7f835eeb4c4bf8d196
Xavi Aracil <xavi@xadsolutions.com>
2012-06-19 22:09:22 +0200

    Develop spec

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe MoviesController do
   describe 'find movies with same director' do
     it 'should call the model method that search movies with given director' do
-      post :same_director, {:id => 1}
+      movie = FactoryGirl.build(:movie)
+      Movie.should_receive(:find_movie_with_same_director).with(movie.id)
+      post :same_director, {:id => movie.id}
     end
     it 'should select the similar movies template for rendering'
     it 'should make the search results to that template'
