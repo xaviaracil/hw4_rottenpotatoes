84e24157e054f5df8c7527c000b21f58a50ffa80
Xavi Aracil <xavi@xadsolutions.com>
2012-06-20 16:09:21 +0200

    Model spec redefined

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Movie do
   describe 'find movies with same director' do
-    it 'should call find method on Movie class'
-    it 'should pass director\'s name as a condition to find method'
-    it 'should return matching movies'
+    it 'should call find method on Movie class with director' do
+      movie = FactoryGirl.build(:movie)
+      Movie.should_receive(:find_all_by_director).with(hash_including :director => 'George Lucas')
+      movie.find_with_same_director
+    end
   end
 end
