45f9c40e91b009e1742722220b62bbc464c0ef21
Xavi Aracil <xavi@xadsolutions.com>
2012-06-20 16:39:28 +0200

    Model specs tested Model method implemented

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 describe Movie do
   describe 'find movies with same director' do
+    before :each do
+      @movie = FactoryGirl.build(:movie)
+    end
     it 'should call find method on Movie class with director' do
-      movie = FactoryGirl.build(:movie)
-      Movie.should_receive(:find_all_by_director).with(hash_including :director => 'George Lucas')
-      movie.find_with_same_director
+      Movie.should_receive(:find_all_by_director).with(hash_including :conditions => hash_including(:director => 'George Lucas'))
+      @movie.find_with_same_director
+    end
+    it 'should return an empty array if movie hasn\'t any director' do
+      @movie.find_with_same_director.should be_empty
     end
   end
 end
