9edfff7584b9abd2b39d4285fc63ea66c63e9724
Xavi Aracil <xavi@xadsolutions.com>
2012-06-20 12:23:10 +0200

    Model spec defined

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Movie do
+  describe 'find movies with same director' do
+    it 'should call find method on Movie class'
+    it 'should pass director\'s name as a condition to find method'
+    it 'should return matching movies'
+  end
+end
